a5fbb981b64a67e81bb94d8e56e4e20ddb318aa6
Matthew Idungafa <mattidungafa@gmail.com>
2024-01-21 19:53:20 +0100

    initial commit

---
+++ conditional-rendering/babel.config.js
@@ -0,0 +1,8 @@
+// .babelrc or babel.config.js
+{
+    "plugins": [
+        "@babel/plugin-proposal-private-property-in-object"
+        // ... other plugins
+    ]
+}
+  
---
+++ conditional-rendering/eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    parser: 'babel-eslint',
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    plugins: ['react'],
+    rules: {
+      // Add your ESLint rules here
+    },
+  };
