f7129cc0d84f4ed89703ed00ebe062127eec589a
Matthew Idungafa <mattidungafa@gmail.com>
2024-01-26 14:08:48 +0100

    added the steps

--- date_counter/date_counter/src/App.js
+++ date_counter/date_counter/src/App.js
@@ -1,34 +1,46 @@
-import { useState } from 'react';
-
+import { useState } from 'react'
 
 export default function App() {
   return (
     <div className="App">
       <Counter />
     </div>
-  );
+  )
 }
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+  const [step, setStep] = useState(1)
 
-  const date = new Date("Jan 26 2023");
-  date.setDate(date.getDate() + count);
+  const date = new Date('Jan 26 2023')
+  date.setDate(date.getDate() + count)
 
   return (
-    <div> 
-      <div style={{marginLeft: 'auto', marginRight: 'auto'}}> 
-        <button onClick={() => setCount((c) => c - 1)}> - </button>
-        <span>Count: {count} </span>
-        <button onClick={() => setCount((c) => c + 1)}> + </button>
+    <div>
+      <div style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+        <button onClick={() => setStep((c) => c - 1)}> - </button>
+        <span>Step: {step} </span>
+        <button onClick={() => setStep((c) => c + 1)}> + </button>
       </div>
 
-      <p> 
-        <span>{count === 0 ? "Today is ": count > 0 ? `${count} days from today is`: `${count} days`} </span>
-        <span>{date.toDateString()}</span>
+      <div style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+        <button onClick={() => setCount((c) => c - step)}> - </button>
+        <span>Count: {count} </span>
+        <button onClick={() => setCount((c) => c + step)}> + </button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? 'Today is '
+            : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+               {' '}
+        </span>
+         <span>{date.toDateString()}</span>
       </p>
     </div>
   )
 }
-
 
