e858586916bf73d62f8c866a17766679736fe751
Matthew Idungafa <mattidungafa@gmail.com>
2024-03-04 01:01:26 +0100

    updates

--- splitter_app/src/App.js
+++ splitter_app/src/App.js
@@ -160,21 +160,41 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState('')
+  const [paidByuser, setPaidByUser] = useState('')
+  const paidByFriend = bill ? bill - paidByuser : ''
+  const [whoIsPaying, setWhoIsPaying] = useState('user')
+
   return (
     <form className="form-split-bill">
       <h2> Split a bill with {selectedFriend.name} </h2>
 
       <label>💰 Bill value </label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label> Your expenses </label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByuser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByuser : Number(e.target.value)
+          )
+        }
+      />
 
       <label> {selectedFriend.name}'s expenses </label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>Who is paying the bill </label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user"> You </option>
         <option value="friend"> X </option>
       </select>
