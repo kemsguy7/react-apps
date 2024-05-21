19a206a0e8aa99a2a498517349a17d5ed09fe830
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-21 14:54:37 +0100

    updates

--- popcorn/src/App.js
+++ popcorn/src/App.js
@@ -170,9 +170,6 @@ export default function App() {
   function WatchedBox() {
     const [watched, setWatched] = useState(tempWatchedData);
     const [isOpen2, setIsOpen2] = useState(true);
-    const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-    const avgUserRating = average(watched.map((movie) => movie.userRating));
-    const avgRuntime = average(watched.map((movie) => movie.runtime));
 
     return <div className="box">
     <button
@@ -183,27 +180,7 @@ export default function App() {
     </button>
     {isOpen2 && (
       <>
-        <div className="summary">
-          <h2>Movies you watched</h2>
-          <div>
-            <p>
-              <span>#️⃣</span>
-              <span>{watched.length} movies</span>
-            </p>
-            <p>
-              <span>⭐️</span>
-              <span>{avgImdbRating}</span>
-            </p>
-            <p>
-              <span>🌟</span>
-              <span>{avgUserRating}</span>
-            </p>
-            <p>
-              <span>⏳</span>
-              <span>{avgRuntime} min</span>
-            </p>
-          </div>
-        </div>
+        <WatchedBox watched={watched} />
 
         <ul className="list">
           {watched.map((movie) => (
@@ -231,4 +208,33 @@ export default function App() {
     )}
   </div>
  
+ }
+
+ function WatchedSummary({ watched }) {
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+  return <div className="summary">
+    <h2>Movies you watched</h2>
+    <div>
+      <p>
+        <span>#️⃣</span>
+        <span>{watched.length} movies</span>
+      </p>
+      <p>
+        <span>⭐️</span>
+        <span>{avgImdbRating}</span>
+      </p>
+      <p>
+        <span>🌟</span>
+        <span>{avgUserRating}</span>
+      </p>
+      <p>
+        <span>⏳</span>
+        <span>{avgRuntime} min</span>
+      </p>
+    </div>
+</div>
+
  }
