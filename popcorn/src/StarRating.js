37cc2d5727c3974ec68ce16b4061b3d982c14865
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-22 01:01:18 +0100

    updatees

--- popcorn/src/StarRating.js
+++ popcorn/src/StarRating.js
@@ -1,3 +1,9 @@
-export default function() StarRating {
-    
+export default function StarRating() {
+
+    return <div> 
+        <div>
+            {Array.from({length: 5}, (_, i) => (<span> S{i+1}</span>))}    
+         </div>
+        <p> 10 </p>
+    </div>
 }
