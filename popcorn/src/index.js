9e1c2591b49e3dbbcec970b204256251cb8eb59e
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-12 16:52:19 +0100

    updates

--- popcorn/src/index.js
+++ popcorn/src/index.js
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom/client'
 
 import StarRating from './StarRating'
 
+function Test() {
+  return <StarRating color="blue" />
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
