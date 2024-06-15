df705bb86d1e3ab8ac4fe220afe6b17ab52feb5c
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-15 08:28:25 +0100

    using callabacks

--- hrw/src/App.js
+++ hrw/src/App.js
@@ -66,8 +66,28 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0)
 
   function handleInc() {
+    //increament likes by 1
     setLikes(likes + 1)
   }
+
+  function handleTripleInc() {
+    //increment likes by 3
+    // setLikes(likes + 1)
+    // setLikes(likes + 1)
+    // setLikes(likes + 1)
+
+    //the above will not work, use callback function to get the previous state
+    setLikes((prevLikes) => prevLikes + 1)
+    setLikes((prevLikes) => prevLikes + 1)
+    setLikes((prevLikes) => prevLikes + 1)
+  }
+
+  function handleUndo() {
+    setShowDetails(true)
+    setLikes(0)
+  }
+
+  function handleUndoIn2s() {}
 
   return (
     <div className="tab-content">
@@ -82,12 +102,12 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
+        <button onClick={handleUndo}> Undo</button>
         <button>Undo in 2s</button>
       </div>
     </div>
