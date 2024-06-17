913e48e30a3d4b2ea744eb446eba6198f4a74879
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-17 17:56:19 +0100

    updates on search synchronization

--- popcorn/src/App.js
+++ popcorn/src/App.js
@@ -53,7 +53,7 @@ const average = (arr) =>
 const KEY = 'f84fc31d' //defining the API key
 
 export default function App() {
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState('inception')
   const [movies, setMovies] = useState([]) // Managing movies state
   const [watched, setWatched] = useState([])
   const [isLoading, setIsLoading] = useState(false)
@@ -61,33 +61,46 @@ export default function App() {
 
   const tempQuery = 'interstellar'
 
-  useEffect(function () {
-    async function fetchMovies() {
-      try {
-        setIsLoading(true) // Loading is set to true while data is being fetched
-        setError('') //reset the error state
-        const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-        )
+  useEffect(
+    function () {
+      async function fetchMovies() {
+        try {
+          setIsLoading(true) // Loading is set to true while data is being fetched
+          setError('') //reset the error state
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          )
 
-        //error handling
-        if (!res.ok)
-          throw new Error('Something went wrong with fetching movies')
+          //error handling
+          if (!res.ok)
+            throw new Error('Something went wrong with fetching movies')
 
-        const data = await res.json()
-        if (data.Response === 'False') throw new Error('Movie not found') //if the request not found
+          const data = await res.json()
+          if (data.Response === 'False') throw new Error('Movie not found') //if the request not found
 
-        setMovies(data.Search)
-        console.log(data.Search)
-      } catch (err) {
-        console.error(err.message)
-        setError(err.message)
-      } finally {
-        setIsLoading(false)
+          setMovies(data.Search)
+          console.log(data.Search)
+        } catch (err) {
+          console.error(err.message)
+          setError(err.message)
+        } finally {
+          setIsLoading(false)
+        }
       }
-    }
-    fetchMovies()
-  }, []) // useEffect makes the fuction not to run while the component is being rendered but after it has been painted to the screen
+
+      if (query.length < 2) {
+        // if search characters is less than 2
+
+        //if no movie has been found. This will prevent the fetchMovies function below from running
+        setMovies([])
+        setError('')
+        return
+      }
+
+      fetchMovies()
+    },
+    [query]
+  ) // useEffect makes the fuction not to run while the component is being rendered but after it has been painted to the screen
 
   return (
     <>
