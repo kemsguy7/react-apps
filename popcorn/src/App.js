0260a46a5e5d6f72f47973716cd300bc30e27d40
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-19 13:25:24 +0100

    cleaned up effects

--- popcorn/src/App.js
+++ popcorn/src/App.js
@@ -83,12 +83,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController() //abort controller to cancel the fetch request
+
       async function fetchMovies() {
         try {
           setIsLoading(true) // Loading is set to true while data is being fetched
           setError('') //reset the error state
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: AbortController.signal }
           )
 
           //error handling
@@ -99,10 +102,14 @@ export default function App() {
           if (data.Response === 'False') throw new Error('Movie not found') //if the request not found
 
           setMovies(data.Search)
+          setError('') // reset the error state
           console.log(data.Search)
         } catch (err) {
           console.error(err.message)
-          setError(err.message)
+
+          if (err.name !== 'AbortError') {
+            setError(err.message)
+          }
         } finally {
           setIsLoading(false)
         }
@@ -114,10 +121,14 @@ export default function App() {
         //if no movie has been found. This will prevent the fetchMovies function below from running
         setMovies([])
         setError('')
-        return
       }
 
       fetchMovies()
+
+      return function () {
+        //return to prevent the fetchMovies function from running
+        controller.abort()
+      }
     },
     [query]
   ) // useEffect makes the fuction not to run while the component is being rendered but after it has been painted to the screen
@@ -360,9 +371,12 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 
   useEffect(
     function () {
-      if (!title) return
-      //this efect will be used to dynamically change the title of the movie
+      if (!title) return //this efect will be used to dynamically change the title of the movie
       document.title = `Movie | ${title}`
+
+      return function () {
+        document.title = 'Popcorn' //reset the title of the page when the component is unmounted(back button is clicked)
+      }
     },
     [title]
   )
