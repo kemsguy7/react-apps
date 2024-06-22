b38cd567436409903dc4038693aca2fe57d3fbee
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-22 03:12:22 +0100

    finished currency converter app

--- currency/src/App.js
+++ currency/src/App.js
@@ -7,37 +7,44 @@ export default function App() {
   const [amount, setAmount] = useState(1);
   const [fromCur, setFromCur] = useState('EUR');
   const [toCur, setToCur] = useState("USD")
+  const [converted, setConverted] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(function() {
     async function convert() {
+      setIsLoading(true)
       const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
-
       )
       const data = await res.json();
-      console.log(data.rates[toCur]);
+      setConverted(data.rates[toCur])
+      setIsLoading(false)
     }
-    convert()
-  }, [])
+
+
+    if (fromCur === toCur || toCur === fromCur  ) return setConverted(amount);  //if both currencies are the same
+    convert() 
+  }, [amount, fromCur, toCur])
 
   return (
     <div>
       <input type="text" 
       value={amount}
       onChange={(e) => setAmount(Number(e.target.value))}
+      disabled={isLoading}
       />
-      <select value ={fromCur} onChange={(e) => setFromCur(e.target.value)}>
+      <select value ={fromCur} onChange={(e) => setFromCur(e.target.value) } disabled={isLoading}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value ={toCur} onChange={(e) => setFromCur(e.target.value)}>
+      <select value ={toCur} onChange={(e) => setFromCur(e.target.value)} disabled={isLoading}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{converted} {toCur} </p>
     </div>
   );
 }
