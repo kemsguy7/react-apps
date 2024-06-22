bcacc6b09c66f337c8eb21a644215fa5edc9e5ad
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-22 02:36:31 +0100

    added currency API

--- currency/src/App.js
+++ currency/src/App.js
@@ -1,16 +1,37 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
- trd
+ 
+import { useEffect, useState } from "react";
+
 export default function App() {
+
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState('EUR');
+  const [toCur, setToCur] = useState("USD")
+
+  useEffect(function() {
+    async function convert() {
+      const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+
+      )
+      const data = await res.json();
+      console.log(data.rates[toCur]);
+    }
+    convert()
+  }, [])
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input type="text" 
+      value={amount}
+      onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select value ={fromCur} onChange={(e) => setFromCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select value ={toCur} onChange={(e) => setFromCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
