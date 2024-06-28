eda4e81c3c1d5d334fb816665a7cecf1a94eee27
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-28 16:35:22 +0100

    Loading data from fake APi in quiz app and implemetating useReducer hook

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,10 +1,38 @@
 //import DateCounter from "./DateCounter";
-import Header from "./Header";
-import Main from "./Main";
+import { useEffect, useReducer } from 'react'
+import Header from './Header'
+import Main from './Main'
+
+const initialState = {
+  question: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataRecieved':
+      return { ...state, question: action.payload, status: 'ready' }
+    case 'dataFailed':
+      return { ...state, status: 'error' }
+    default:
+      throw new Error('Action unknown')
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataRecieved', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }))
+  }, [])
+
   return (
-    <div className="app"> 
+    <div className="app">
       <Header />
 
       <Main>
@@ -12,7 +40,5 @@ export default function App() {
         <p>Question?</p>
       </Main>
     </div>
-
-   
   )
 }
