5d8485a714744458a46a17e0e97c899da26b9fdf
Matthew Idungafa <mattidungafa@gmail.com>
2024-07-20 10:17:49 +0100

    updates: updated the startscreen component

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -20,6 +20,8 @@ function reducer(state, action) {
       return { ...state, question: action.payload, status: 'ready' }
     case 'dataFailed':
       return { ...state, status: 'error' }
+    case 'start':
+      return { ...state, status: 'active' }
     default:
       throw new Error('Action unknown')
   }
@@ -45,7 +47,9 @@ export default function App() {
         {' '}
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
         {status === 'active' && <Question />}
       </Main>
     </div>
--- quiz-app/src/StartScreen.js
+++ quiz-app/src/StartScreen.js
@@ -1,9 +1,15 @@
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2> Welcome to The React Quiz </h2>
       <h1> {numQuestions} questions to test your React mastery </h1>
-      <button className="btn btn-ui"> Let's start </button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'start' })}
+      >
+        {' '}
+        Let's start{' '}
+      </button>
     </div>
   )
 }
