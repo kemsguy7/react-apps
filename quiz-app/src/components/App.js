68c0d818658e53cbeec4bf820e7a258a0e34a15b
Matthew Idungafa <mattidungafa@gmail.com>
2024-07-20 18:58:26 +0100

    updates: handling new answers

--- quiz-app/src/components/App.js
+++ quiz-app/src/components/App.js
@@ -6,6 +6,7 @@ import Loader from './Loader'
 import Error from './Error'
 import StartScreen from './StartScreen'
 import Question from './Question'
+import Progress from './Progress'
 
 const initialState = {
   questions: [],
@@ -13,6 +14,7 @@ const initialState = {
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: 'loading',
   index: 0,
+  answer: null, // answer is null by default
 }
 
 function reducer(state, action) {
@@ -23,13 +25,15 @@ function reducer(state, action) {
       return { ...state, status: 'error' }
     case 'start':
       return { ...state, status: 'active' }
+    case 'newAnswer':
+      return { ...state, answer: action.payload }
     default:
       throw new Error('Action unknown')
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   ) // destructuring the state object
@@ -55,7 +59,16 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions[index]} />}
+        {status === 'active' && (
+          <>
+            <Progress index={index} numQuestions={numQuestions} />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+          </>
+        )}
       </Main>
     </div>
   )
